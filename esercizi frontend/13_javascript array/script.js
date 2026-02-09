d707eb2ea673653f9e43194f047cd83539acf987
Giuseppe-Midolo <giuseppemidolo@outlook.it>
2026-02-09 15:01:44 +0100

    Delete esercizi frontend directory

--- esercizi frontend/13_javascript array/script.js
+++
@@ -1,2 +0,0 @@
-var array = [1, 2, 3, 4, 5];
-console.log(array[3]); // This will throw an error because arrays are accessed with square brackets, not parentheses.
--- esercizi frontend/17/app.js
+++
@@ -1,8 +0,0 @@
-const button = document.getElementById("button");
-const titles = document.querySelectorAll("div h1");
-
-button.addEventListener("click", function () {
-    titles.forEach(function (h1) {
-        h1.classList.toggle("blue");
-    });
-});
